315a75cfa2998dc884f918d7de83716ccc04b2c5
jcontreras28 <janellec@bendbroadband.com>
2017-12-26 15:31:17 -0800

    added emit new message on create

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Fred',
-        text: 'This is the message body'
-    });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from: 'Wilma',
-        text: 'Hello world!!!  Whats up?',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (newMessage) => {
         console.log('Create new message', newMessage);
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', (socket) => {
