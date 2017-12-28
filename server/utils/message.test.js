946f8f7c03c9031f11197f467d6e4ce2ee3e93f9
jcontreras28 <janellec@bendbroadband.com>
2017-12-27 16:40:03 -0800

    added css and user exp to chat page

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,6 @@
 var expect = require('expect');
 var {generateMessage} = require('./message');
+var {generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -11,6 +12,23 @@ describe('generateMessage', () => {
         expect(message).toInclude({
             from: from,
             text: text
+        });
+
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate the correct message object', () => {
+        var from = 'Me';
+        var lat = '55';
+        var long = '77';
+        var url = `https://www.google.com/maps?q=${lat},${long}`;
+        var message = generateLocationMessage(from, lat, long);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({
+            from: from,
+            url: url
         });
 
     });
